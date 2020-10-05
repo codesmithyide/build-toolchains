2f01b28cab8689885befec276da4f684702b1d20
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-05 23:08:43 +0100

    Updates

---
+++ Include/CodeSmithy/BuildToolchains/VisualStudioToolchain.h
@@ -0,0 +1,19 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/CodeSmithyIDE/BuildToolchains/blob/master/LICENSE.txt
+*/
+
+#ifndef _CODESMITHYIDE_BUILDTOOLCHAINS_VISUALSTUDIOTOOLCHAIN_H_
+#define _CODESMITHYIDE_BUILDTOOLCHAINS_VISUALSTUDIOTOOLCHAIN_H_
+
+namespace CodeSmithy
+{
+
+class VisualStudioToolchain
+{
+};
+
+}
+
+#endif
