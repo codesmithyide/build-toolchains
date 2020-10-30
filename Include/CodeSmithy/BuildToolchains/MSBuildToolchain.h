51da486ba95b7a10a3552f525492d662cded67bd
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-30 23:34:58 +0000

    Replace Visual Studio tooling with MSBuild (#5)

---
+++ Include/CodeSmithy/BuildToolchains/MSBuildToolchain.h
@@ -0,0 +1,36 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/CodeSmithyIDE/BuildToolchains/blob/master/LICENSE.txt
+*/
+
+#ifndef _CODESMITHYIDE_BUILDTOOLCHAINS_MSBUILDTOOLCHAIN_H_
+#define _CODESMITHYIDE_BUILDTOOLCHAINS_MSBUILDTOOLCHAIN_H_
+
+#include <Ishiko/Errors.h>
+#include <Ishiko/Process.h>
+#include <string>
+
+namespace CodeSmithy
+{
+
+class MSBuildToolchain
+{
+public:
+    MSBuildToolchain();
+
+    void build(const std::string& makefilePath) const;
+    void build(const std::string& makefilePath, Ishiko::Error& error) const noexcept;
+    void build(const std::string& makefilePath, const Ishiko::Process::Environment& environment) const;
+    void build(const std::string& makefilePath, const Ishiko::Process::Environment& environment,
+        Ishiko::Error& error) const noexcept;
+
+private:
+    std::string m_msbuildPath;
+};
+
+}
+
+#include "linkoptions.h"
+
+#endif
