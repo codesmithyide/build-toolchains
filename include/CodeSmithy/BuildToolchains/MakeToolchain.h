97d3d1e40b74e3dabbb0450f7eb2f395ff03ac35
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-17 17:08:29 +0100

    Update code after changes to dependencies

--- include/CodeSmithy/BuildToolchains/MakeToolchain.h
+++ include/CodeSmithy/BuildToolchains/MakeToolchain.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2020 Xavier Leclercq
+    Copyright (c) 2020-2022 Xavier Leclercq
     Released under the MIT License
     See https://github.com/codesmithyide/build-toolchains/blob/main/LICENSE.txt
 */
@@ -7,7 +7,7 @@
 #ifndef _CODESMITHYIDE_BUILDTOOLCHAINS_MAKETOOLCHAIN_H_
 #define _CODESMITHYIDE_BUILDTOOLCHAINS_MAKETOOLCHAIN_H_
 
-#include <Ishiko/Process.h>
+#include <Ishiko/Process.hpp>
 #include <string>
 
 namespace CodeSmithy
@@ -18,7 +18,7 @@ class MakeToolchain
 public:
     MakeToolchain();
 
-    void build(const std::string& makefilePath, const Ishiko::Process::Environment& environment) const;
+    void build(const std::string& makefilePath, const Ishiko::Environment& environment) const;
 
 private:
     std::string m_makePath;
