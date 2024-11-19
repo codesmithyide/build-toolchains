19bda213241fff322e502c4b5224852d63d96313
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-19 14:32:05 +0000

    Updates

--- tests/src/CMakeToolchainTests.cpp
+++ tests/src/CMakeToolchainTests.cpp
@@ -3,34 +3,58 @@
 
 #include "CMakeToolchainTests.h"
 #include "CodeSmithy/BuildToolchains/CMakeToolchain.h"
+#include <Ishiko/BasePlatform.hpp>
 #include <Ishiko/Process.hpp>
 #include <boost/filesystem.hpp>
 
-using namespace Ishiko;
+using namespace CodeSmithy;
 
-CMakeToolchainTests::CMakeToolchainTests(const TestNumber& number, const TestContext& context)
-    : TestSequence(number, "CMakeToolchain tests", context)
+CMakeToolchainTests::CMakeToolchainTests(const Ishiko::TestNumber& number, const Ishiko::TestContext& context)
+    : Ishiko::TestSequence(number, "CMakeToolchain tests", context)
 {
-    append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
-    append<HeapAllocationErrorsTest>("generate test 1", GenerateTest1);
+    append<Ishiko::HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
+    append<Ishiko::HeapAllocationErrorsTest>("generate test 1", GenerateTest1);
+    append<Ishiko::HeapAllocationErrorsTest>("generate test 2", GenerateTest2);
 }
 
-void CMakeToolchainTests::ConstructorTest1(Test& test)
+void CMakeToolchainTests::ConstructorTest1(Ishiko::Test& test)
 {
-    CodeSmithy::CMakeToolchain toolchain;
+    CMakeToolchain toolchain;
 
     ISHIKO_TEST_PASS();
 }
 
-void CMakeToolchainTests::GenerateTest1(Test& test)
+void CMakeToolchainTests::GenerateTest1(Ishiko::Test& test)
 {
-    const boost::filesystem::path& cmakelists_path = test.context().getOutputPath("cmake/hello-world/CMakeLists.txt");
+    const boost::filesystem::path& cmakelists_path = test.context().getOutputPath("GenerateTest1/CMakeLists.txt");
     boost::filesystem::create_directories(cmakelists_path.parent_path());
     test.utils().copyFile("${context.data}/cmake/hello-world/CMakeLists.txt", cmakelists_path.string());
-    test.utils().copyFile("${context.data}/cmake/hello-world/main.cpp", "${context.output}/cmake/hello-world/main.cpp");
+    test.utils().copyFile("${context.data}/cmake/hello-world/main.cpp", "${context.output}/GenerateTest1/main.cpp");
 
-    CodeSmithy::CMakeToolchain toolchain;
+    CMakeToolchain toolchain;
     toolchain.generate(cmakelists_path.string(), Ishiko::CurrentEnvironment());
+
+    ISHIKO_TEST_PASS();
+}
+
+void CMakeToolchainTests::GenerateTest2(Ishiko::Test& test)
+{
+    const boost::filesystem::path& cmakelists_path = test.context().getOutputPath("GenerateTest2/CMakeLists.txt");
+    boost::filesystem::create_directories(cmakelists_path.parent_path());
+    test.utils().copyFile("${context.data}/cmake/hello-world/CMakeLists.txt", cmakelists_path.string());
+    test.utils().copyFile("${context.data}/cmake/hello-world/main.cpp", "${context.output}/GenerateTest2/main.cpp");
+
+    CMakeToolchain toolchain;
+
+#if ISHIKO_OS == ISHIKO_OS_LINUX
+    CMakeGenerationOptions options("Unix Makefiles", "x64", {});
+#elif ISHIKO_OS == ISHIKO_OS_WINDOWS
+    CMakeGenerationOptions options("Visual Studio 17 2022", "x64", {});
+#else
+#error Unsupported or unrecognized OS
+#endif
+
+    toolchain.generate(cmakelists_path.string(), options, Ishiko::CurrentEnvironment());
 
     ISHIKO_TEST_PASS();
 }
